0939ccbf7244054c8e3d9f7d67055ff03899f114
Ant Devs <55980467+Ant-Devs@users.noreply.github.com>
2021-02-21 22:26:27 +0700

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,64 @@
+float c = 261.63;
+float d = 293.665;
+float e = 329.628;
+float g = 391.995;
+
+int pin = pin;
+
+void setup() {
+  pinMode(pin, OUTPUT);
+  lamb();
+}
+
+void lamb(){
+  tone(pin, e, 500);
+  delay(500);
+  tone(pin, d, 500);
+  delay(500);
+  tone(pin, c, 500);
+  delay(500);
+  tone(pin, d, 500);
+  delay(500);
+  tone(pin, e, 500);
+  delay(500);
+  tone(pin, e, 500);
+  delay(500);
+  tone(pin, e, 500);
+  delay(900);
+  tone(pin, d, 500);
+  delay(500);
+  tone(pin, d, 500);
+  delay(500);
+  tone(pin, d, 500);
+  delay(900);
+  tone(pin, e, 500);
+  delay(500);
+  tone(pin, g, 500);
+  delay(500);
+  tone(pin, g, 500);
+  delay(900);
+  tone(pin, e, 500);
+  delay(500);
+  tone(pin, d, 500);
+  delay(500);
+  tone(pin, c, 500);
+  delay(500);
+  tone(pin, d, 500);
+  delay(500);
+  tone(pin, e, 500);
+  delay(500);
+  tone(pin, e, 500);
+  delay(500);
+  tone(pin, e, 500);
+  delay(900);
+  tone(pin, d, 500);
+  delay(500);
+  tone(pin, d, 500);
+  delay(500);
+  tone(pin, e, 500);
+  delay(500);
+  tone(pin, d, 500);
+  delay(500);
+  tone(pin, c, 500);
+  delay(500);
+}
